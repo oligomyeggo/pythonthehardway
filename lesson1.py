3b029971fbd9d6fd0e68601e016ef125a08c263e
oligomyeggo <winklercaitlin@gmail.com>
2015-09-12 11:36:29 -0600

    Add print statements.

---
+++ lesson1.py
@@ -0,0 +1,7 @@
+print "Hello World!"
+print "Hello Again"
+print "Fuck you"
+print "This is not that much fun"
+print "Stupid fucking printing is fucking boring and easy"
+print "I'd much rather you 'not'. ...Why? What?"
+print 'I "said" do not touch this.'
