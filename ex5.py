6d227e213dd004056bdea72a8b6990e9679c9c35
oligomyeggo <winklercaitlin@gmail.com>
2015-09-14 15:46:13 -0600

    Added exercise 5 for Learn Python the Hard Way - More Variables and Printing.

---
+++ ex5.py
@@ -0,0 +1,17 @@
+my_name = 'Caitlin Fury'
+my_age = 25 # not a lie
+my_height = 66 # inches
+my_weight = 135 # lbs; this is a lie
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Blonde'
+
+print "Let's talk about %s." % my_name
+print "She's %d inches tall." % my_height
+print "She's %d pounds heavy." % my_weight
+print "Actually that's not too heavy."
+print "She's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "Her teeth are usually %s depending on the coffee." % my_teeth
+
+print "If I add %d, %d, and %d I get %d." % (
+    my_age, my_height, my_weight, my_age + my_height + my_weight)
