42ad836f4984ea9070602b834f6df5fd2fc624be
oligomyeggo <winklercaitlin@gmail.com>
2015-09-14 14:04:51 -0600

    Added exercise 3 for Learn Python the Hard Way - Numbers and Math.

---
+++ ex3.py
@@ -0,0 +1,30 @@
+print "I will now count my chickens:"
+
+print "Hens", 25.0+30.0/6.0
+print "Roosters", 100.0-25.0*3.0%4.0
+# The % sign means 'modulus'.
+# When the left hand operand is divided by the right hand operand, the remainder is returned.
+# In this instance, 25*3 = 75, 75 % 4 = 3. 100 - 3 = 97.
+
+print "Now I will count the eggs:"
+
+print 3.0+2.0+1.0-5.0+4.0%2.0-1.0/4.0+6.0
+
+print "Is it true that 3 + 2 < 5 - 7?"
+
+print 3.0+2.0<5.0-7.0
+
+print "What is 3 + 2?", 3.0+2.0
+print "What is 5 - 7?", 5.0-7.0
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+print "Is it greater?", 5.0 > -2.0
+print "Is it greater or equal?", 5.0 >= -2.0
+print "Is it less or equal?", 5.0 <= -2.0
+
+print "Do I have floating point numbers?"
+print 3.0/2.0
+print "Yes!"
